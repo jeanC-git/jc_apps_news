079cb17b299a61661875ffbb59bb7bf1eba48b88
Jean Carlo <jeancarlo@lamediadl.com>
2021-03-24 14:42:35 -0500

    avance pageview dinamico

--- lib/src/pages/TabsPage.dart
+++ lib/src/pages/TabsPage.dart
@@ -1,31 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _Navegacion();
+    return ChangeNotifierProvider(
+        create: (_) => new _NavegacionModel(),
+        child: Scaffold(bottomNavigationBar: _Navegacion(), body: _Paginas()));
   }
 }
 
 class _Navegacion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
     return Scaffold(
         bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0,
-          onTap: (i) => print('$i'),
-          items: [
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.person_outline_outlined,
-              ),
-              label: 'Para ti'),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.public,
-              ),
-              label: 'Encabezados'),
-        ]),
+            currentIndex: navegacionModel.paginaActual,
+            onTap: (i) => navegacionModel.paginaActual = i ,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.person_outline_outlined,
+                  ),
+                  label: 'Para ti'),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.public,
+                  ),
+                  label: 'Encabezados'),
+            ]),
         body: _Paginas());
   }
 }
@@ -33,7 +38,10 @@ class _Navegacion extends StatelessWidget {
 class _Paginas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
     return PageView(
+      controller: navegacionModel.pageController,
       // physics: BouncingScrollPhysics(),
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
@@ -48,12 +56,18 @@ class _Paginas extends StatelessWidget {
   }
 }
 
+class _NavegacionModel with ChangeNotifier {
 
-class _NavegacionModel with ChangeNotifier{
-  int _paginaActual =  0;
+  int _paginaActual = 0;
+
+  PageController _pageController = new PageController();
+
   int get paginaActual => this._paginaActual;
-  set paginaActual(int valor){
+  set paginaActual(int valor) {
     this._paginaActual = valor;
+    _pageController.animateToPage(valor, duration: Duration(milliseconds: 250) , curve: Curves.easeOut);
     notifyListeners();
   }
+
+  PageController get pageController => this._pageController;
 }
